a5b1c39710ded4ed030940f22d3844612a4c9d3c
S S Chowdary Alapati <110979676+Chowdary225@users.noreply.github.com>
2025-11-29 12:43:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+// export default defineConfig({
+//   base: '/portfolio/',
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+const isGhPages = process.env.DEPLOY_TARGET === 'GH_PAGES'  // set this when you build for GH Pages
+const base = isGhPages ? '/portfolio/' : '/'
+
 export default defineConfig({
-  base: '/portfolio/',
+  base,
   plugins: [react()],
 })
